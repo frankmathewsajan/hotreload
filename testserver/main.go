bc10d3f85e1e67c1a0d3358d2d80c62544ac0a4e
Frank Mathew Sajan <frankmathewsajan@gmail.com>
2026-03-06 19:46:49 +0530

    chore: initialize module and dummy test server

---
+++ testserver/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// This print statement is critical. We need to see this in our terminal 
+	// to verify the hot-reload tool is successfully streaming the output.
+	fmt.Println("[TEST SERVER] Booting up on port 8080...")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hot Reload Engine is Operational!\n")
+	})
+
+	// Start the server and listen for requests
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("[TEST SERVER] Fatal error:", err)
+	}
+}
